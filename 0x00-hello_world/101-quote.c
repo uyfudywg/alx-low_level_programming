3874a522ffac52890dfda871d1083b256416686a
uyfudywg <bdaldaymb578@gmail.com>
2023-10-05 10:13:42 -0700

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,18 @@
-#!/bin/bash 
-gss -S -masm=intel $CFILE -o'echo $CFILE | cut -d. -fl'.s
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a quote write function
+ * write(int fd,count void *buf ,size_t count);
+ * Return: 1 (not success).
+ *
+*/
+
+int main(void)
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+
+}
