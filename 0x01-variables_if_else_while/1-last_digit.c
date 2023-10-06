36f3546035475af41440ab2741dd4f884e83df93
uyfudywg <bdaldaymb578@gmail.com>
2023-10-06 02:30:42 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:desctrip nummer (n) is number greater or less from Zero
+ *
+ * Return: alawys 0 (success)
+*/
+
+int main(void)
+{
+	int n, digit;
+
+:	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	digit = n % 10;
+	if (digit > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
+	if (digit < 6 && digit != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
+	if (digit == 0)
+		printf("Last digit of %d is %d and is 0\n", n, digit);
+	return (0);
+}
