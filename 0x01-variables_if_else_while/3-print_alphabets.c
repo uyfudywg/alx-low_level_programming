290ef5c79b7f2b0f00c4a41d29b4e20f053a5485
uyfudywg <bdaldaymb578@gmail.com>
2023-10-09 09:18:26 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * description:print prints the alphabet in lowercase, followed by a new line
+ * Return: alawys 0 (success)
+*/
+int main(void)
+{
+
+	char ch = 'a';
+
+	char cH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar ('\n');
+	while (cH <= 'Z')
+	{
+		putchar(cH);
+		cH++;
+	}
+	putchar ('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: print all aplhabet letters except q and e 
+ *
+ * Return: always 0 (success)
+*/
+int main (void)
+{
+	char ch = 'a';
+
+	while(ch <= 'z')
+	{
+		if (ch=='q' || ch ='e')
+	
+	
+	}
+
+
+
+}
