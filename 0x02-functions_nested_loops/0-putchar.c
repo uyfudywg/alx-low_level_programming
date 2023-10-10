666f2b963ce19262e93df7ea8038aaf283b27a1c
uyfudywg <bdaldaymb578@gmail.com>
2023-10-10 09:15:25 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,30 @@
+
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototyoe
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+
+	int ch;
+
+	for (ch = 0, ch < 8, ch++)
+		_putchar (str[ch]);
+	_putchar('\n');
+
+	return (0);
+
+
+
+
+
+
+
+}
