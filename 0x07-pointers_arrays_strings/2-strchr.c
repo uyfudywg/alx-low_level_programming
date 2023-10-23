3cae3a30c36493820eda8afc10218a915f3a6902
uyfudywg <bdaldaymb578@gmail.com>
2023-10-23 08:42:00 -0700

    0x07

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - fille memory with a constant byte.
+ * @s: pointer to put the constant
+ * @c: constant
+ *  Return: pointer s
+*/
+
+char *_strchr(char *s, char c);
+{
+	int it;
+
+	for (it = 0; s[it] >= '\0'; it++)
+	{
+		if (s[it] == c)
+		{
+			return (s + it);
+		}
+
+	}
+
+	return ('\0');
+
+}
