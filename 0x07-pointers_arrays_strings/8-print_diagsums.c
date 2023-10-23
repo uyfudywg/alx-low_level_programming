73bbd29b35f5ad752d4518568cbe6ecaed4498c9
uyfudywg <bdaldaymb578@gmail.com>
2023-10-23 10:14:03 -0700

    0x07

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - desctiption
+ * @a: 2d array of int types
+ * @size: size of array
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int it, t1 = 0, t2 = 0;
+
+	for (it = 0; it < size; it++)
+	{
+		t1 += a[it];
+		t2 += a[size - it - 1];
+		a += size;
+	}
+	printf("%d, ", s1);
+	printf("%d\n, ", s2);
+}
